c898e8d44b0030e03cd8720c30ed36ba03e80c79
Tamil Arsen <TamilArsen88@gmail.com>
2026-09-10 23:47:46 +0700

    chore: finalize portfolio configuration

--- src/data/site.ts
+++ src/data/site.ts
@@ -89,6 +89,6 @@ export const siteConfig = {
   assets: {
     favicon: "/favicon.svg",
     // ogImage: "/og-image.png",
-    resume: "/resume.pdf",
+    resume: "resume/resume.pdf",
   },
 } as const;
